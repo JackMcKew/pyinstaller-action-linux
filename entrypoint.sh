212ad8a1746bec45a8b785cdc75407152a18474e
Jack McKew <jackmckew2@gmail.com>
2020-08-10 18:54:01 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -i
 
 # Fail on errors.
 set -e
@@ -25,14 +25,14 @@ if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
    [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
     # the funky looking regexp just extracts the hostname, excluding port
     # to be used as a trusted-host.
-    mkdir -p /wine/drive_c/users/root/pip
-    echo "[global]" > /wine/drive_c/users/root/pip/pip.ini
-    echo "index = $PYPI_URL" >> /wine/drive_c/users/root/pip/pip.ini
-    echo "index-url = $PYPI_INDEX_URL" >> /wine/drive_c/users/root/pip/pip.ini
-    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /wine/drive_c/users/root/pip/pip.ini
+    mkdir -p /root/.pip
+    echo "[global]" > /root/.pip/pip.conf
+    echo "index = $PYPI_URL" >> /root/.pip/pip.conf
+    echo "index-url = $PYPI_INDEX_URL" >> /root/.pip/pip.conf
+    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /root/.pip/pip.conf
 
-    echo "Using custom pip.ini: "
-    cat /wine/drive_c/users/root/pip/pip.ini
+    echo "Using custom pip.conf: "
+    cat /root/.pip/pip.conf
 fi
 
 cd $WORKDIR
@@ -41,11 +41,5 @@ if [ -f requirements.txt ]; then
     pip install -r requirements.txt
 fi # [ -f requirements.txt ]
 
-
-
-# if [[ "$@" == "" ]]; then
 pyinstaller --clean -y --dist ./dist/linux --workpath /tmp *.spec
 chown -R --reference=. ./dist/linux
-# else
-    # sh -c "$@"
-# fi # [[ "$@" == "" ]]
