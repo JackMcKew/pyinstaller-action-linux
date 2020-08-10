a2586f91548d51427520bf8f8279d6d40b91f963
Jack McKew <jackmckew2@gmail.com>
2020-08-10 19:22:26 +1000

    Try full path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
 # Fail on errors.
 set -e
@@ -38,7 +38,7 @@ fi
 cd $WORKDIR
 
 if [ -f requirements.txt ]; then
-    python -m pip install -r requirements.txt
+    /root/.pyenv/shims/pip install -r requirements.txt
 fi # [ -f requirements.txt ]
 
 pyinstaller --clean -y --dist ./dist/linux --workpath /tmp *.spec
