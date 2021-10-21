78c6c17830fb0cad79f2b783796af7938e8c7351
Jean-Emmanuel Longueville <jean.emmanuel.longueville@univ-poitiers.fr>
2021-10-21 13:01:12 +0200

    Bugfix install dependancies

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,11 +43,11 @@ cd $WORKDIR
 
 
 if [[ $7 == "true" ]]; then
-    apt update
-    apt upgrade -y
-    apt install -y libwxbase3.0-0v5 libwxgtk-media3.0-gtk3-0v5 libwxgtk-webview3.0-gtk3-0v5 \
+    apt-get update
+    apt-get upgrade -y
+    apt-get install -y libwxbase3.0-0v5 libwxgtk-media3.0-gtk3-0v5 libwxgtk-webview3.0-gtk3-0v5 \
     libwxgtk3.0-gtk3-0v5 python3-wxgtk-media4.0 python3-wxgtk-webview4.0 python3-wxgtk4.0 \
-    libgtk-3-dev 
+    libgtk-3-dev libjpeg-dev
     # /root/.pyenv/shims/pip install gooey
 fi # [$7]
 
