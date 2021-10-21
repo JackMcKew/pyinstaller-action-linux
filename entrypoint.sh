1abcd03fd3af6d49f3d747de312f13aa3c168e6b
Jean-Emmanuel Longueville <jean.emmanuel.longueville@univ-poitiers.fr>
2021-10-21 09:55:52 +0200

    Modify Yml and sh
    Add the requirement option like in windows action
    Add the install of gooey with option

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ WORKDIR=${SRCDIR:-/src}
 
 SPEC_FILE=${4:-*.spec}
 
-/root/.pyenv/shims/python -m pip install --upgrade pip wheel setuptools
+/root/.pyenv/shims/python -m pip install --upgrade pip wheel setuptools pyenv
 
 #
 # In case the user specified a custom URL for PYPI, then use
@@ -41,9 +41,20 @@ fi
 
 cd $WORKDIR
 
-if [ -f requirements.txt ]; then
-    /root/.pyenv/shims/pip install -r requirements.txt
-fi # [ -f requirements.txt ]
+
+if [$7 == "true"]
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install -y libwxbase3.0-0v5 libwxgtk-media3.0-gtk3-0v5 libwxgtk-webview3.0-gtk3-0v5 \
+    libwxgtk3.0-gtk3-0v5 python3-wxgtk-media4.0 python3-wxgtk-webview4.0 python3-wxgtk4.0 \
+    libgtk-3-dev 
+    /root/.pyenv/shims/pip install gooey
+fi #[$7]
+
+if [ -f $6 ]; then
+    /root/.pyenv/shims/pip install -r $6
+fi # [ -f $6 ]
+
 
 /root/.pyenv/shims/pyinstaller --clean -y --dist ./dist/linux --workpath /tmp $SPEC_FILE
 
